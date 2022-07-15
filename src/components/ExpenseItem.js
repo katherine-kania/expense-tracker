e5d859fb6ffba5f53174a0408abbc415f93611e7
katherine-kania <katherine.kania@gmail.com>
2022-07-15 17:10:00 -0400

    expense props added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,19 @@
-function ExpenseItem() {
-  return (
-    <div>
-      <div>July 15, 2022</div>
-      <div>
-        <h2>Car Insurance</h2>
-        <div>$294.97</div>
-      </div>
-    </div>
-  );
+import './ExpenseItem.css'
+
+function ExpenseItem(props) {
+    // const expenseDate = new Date(2022, 7, 15)
+    // const expenseTitle = 'Car Insurance'
+    // const expenseAmount = 294.67
+    
+    return (
+        <div className="expense-item">
+        <div className="expense-item_description">{props.date.toISOString()}</div>
+        <div>
+            <h2>{props.title}</h2>
+            <div className="expense-item_price">${props.amount}</div>
+        </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
