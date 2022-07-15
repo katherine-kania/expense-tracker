0c56f0e21dcf614a650e28fd421da4104e5407fc
katherine-kania <katherine.kania@gmail.com>
2022-07-15 18:04:17 -0400

    ExpenseDate component created

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,15 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
-    // const expenseDate = new Date(2022, 7, 15)
-    // const expenseTitle = 'Car Insurance'
-    // const expenseAmount = 294.67
     
     return (
         <div className="expense-item">
-        <div className="expense-item_description">{props.date.toISOString()}</div>
-        <div>
-            <h2>{props.title}</h2>
-            <div className="expense-item_price">${props.amount}</div>
-        </div>
+            <ExpenseDate />
+            <div className="expense-item_description">
+                <h2>{props.title}</h2>
+                <div className="expense-item_price">${props.amount}</div>
+            </div>
         </div>
     );
 }
