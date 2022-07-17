32108515f318f79fe7f4a8c41fba82ed611e553e
katherine-kania <katherine.kania@gmail.com>
2022-07-17 19:41:32 -0400

    form added

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,25 @@
 import './ExpenseForm.css'
 
 const ExpenseForm = () => {
-    return
+    return <form>
+        <div className='new-expense_controls'>
+            <div className='new-expense_control'>
+                <label>Title</label>
+                <input type='text' />
+            </div>
+            <div className='new-expense_control'>
+                <label>Amount</label>
+                <input type='number' min='0.01' step='0.01' />
+            </div>
+            <div className='new-expense_control'>
+                <label>Date</label>
+                <input type='date' min='2019-01-01' max='2022-12-31' />
+            </div>
+        </div>
+        <div className='new-expenses_actions'>
+            <button type='submit'>Add Submit</button>
+        </div>
+    </form>
 
 }
 
