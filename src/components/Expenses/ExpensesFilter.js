6cebd7d36cc002d14b23e6a6ab04d138f3ae3f3c
katherine-kania <katherine.kania@gmail.com>
2022-07-18 15:17:21 -0400

    ExpenseFilter reacting

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -1,19 +1,22 @@
 import "./ExpensesFilter.css";
 
-const ExpensesFilter = () => {
-  return (
-    <div className="expenses-filter">
-      <div className="expenses-filter_control">
-        <label>Filter by year</label>
-        <select>
-          <option value="2022">2022</option>
-          <option value="2021">2021</option>
-          <option value="2020">2020</option>
-          <option value="2019">2019</option>
-        </select>
-      </div>
-    </div>
-  );
+const ExpensesFilter = (props) => {
+    const dropDownChangeFilter = (event) => {
+        props.onChangeFilter(event.target.value)
+    }
+    return (
+        <div className="expenses-filter">
+        <div className="expenses-filter_control">
+            <label>Filter by year</label>
+            <select value={props.selected} onChange={dropDownChangeFilter}>
+            <option value="2022">2022</option>
+            <option value="2021">2021</option>
+            <option value="2020">2020</option>
+            <option value="2019">2019</option>
+            </select>
+        </div>
+        </div>
+    );
 };
 
 export default ExpensesFilter
