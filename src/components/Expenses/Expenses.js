914319615dcaa47e21cb9ae1086b67421c01cc37
katherine-kania <katherine.kania@gmail.com>
2022-07-19 11:58:59 -0400

    ExpensesList component and css added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,7 +1,7 @@
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import { useState } from "react";
+import ExpensesList from "./ExpensesList";
 import "./Expenses.css";
 
 const Expenses = (props) => {
@@ -16,20 +16,14 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear
   })
 
+
   return (
       <Card className="expenses">
         <ExpensesFilter 
           selected={filteredYear} 
           onChangeFilter={filterChangeHandler}
         />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id} 
-            title={expense.title} 
-            amount={expense.amount}
-            date={expense.date} 
-          />
-        ))} 
+        <ExpensesList items={filteredExpenses} />
       </Card>
   );
 }
